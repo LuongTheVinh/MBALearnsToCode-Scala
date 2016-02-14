40919a4bf8552cf49fa260e60202131842815aa3
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-02-14 15:59:24 -0600

    add module Expr in package pkg

---
+++ src/main/scala/pkg/expr/expr.scala
@@ -0,0 +1,35 @@
+package pkg.expr
+
+
+trait Expr {
+
+  def eval: Int = this match {
+    case Num(n) => n
+    case Sum(e1, e2) => e1.eval + e2.eval
+    case Prod(e1, e2) => e1.eval * e2.eval
+  }
+
+  def isSum: Boolean = this match {
+    case Num(n) => false
+    case Sum(e1, e2) => true
+    case Prod(e1, e2) => false
+  }
+
+  override def toString = this match {
+    case Num(n) => n.toString
+    case Sum(e1, e2) => e1 + " + " + e2
+    case Prod(e1, e2) => {
+      (if (e1.isSum) "(" + e1 + ")" else e1) + " * " +
+        (if (e2.isSum) "(" + e2 + ")" else e2)
+    }
+  }
+}
+
+
+case class Num(n: Int) extends Expr
+
+
+case class Sum(e1: Expr, e2: Expr) extends Expr
+
+
+case class Prod(e1: Expr, e2: Expr) extends Expr
