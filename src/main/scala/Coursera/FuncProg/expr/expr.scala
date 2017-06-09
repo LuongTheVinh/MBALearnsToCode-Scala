7de3a362a5548dd196eca83229e28e075a68b647
MBALearnsToCode <MBALearnsToCode@UChicago.edu>
2017-06-08 21:38:43 -0700

    minor refactoring

--- src/main/scala/Coursera/FuncProg/expr/expr.scala
+++ src/main/scala/Coursera/FuncProg/expr/expr.scala
@@ -1,28 +1,30 @@
-package pkg.expr
+package Coursera.FuncProg.expr
 
 
 trait Expr {
-
-  def eval: Int = this match {
-    case Num(n) => n
-    case Sum(e1, e2) => e1.eval + e2.eval
-    case Prod(e1, e2) => e1.eval * e2.eval
-  }
-
-  def isSum: Boolean = this match {
-    case Num(n) => false
-    case Sum(e1, e2) => true
-    case Prod(e1, e2) => false
-  }
-
-  override def toString = this match {
-    case Num(n) => n.toString
-    case Sum(e1, e2) => e1 + " + " + e2
-    case Prod(e1, e2) => {
-      (if (e1.isSum) "(" + e1 + ")" else e1) + " * " +
-        (if (e2.isSum) "(" + e2 + ")" else e2)
+  def eval: Int =
+    this match {
+      case Num(n) => n
+      case Sum(e1, e2) => e1.eval + e2.eval
+      case Prod(e1, e2) => e1.eval * e2.eval
     }
-  }
+
+  def isSum: Boolean =
+    this match {
+      case Num(n) => false
+      case Sum(e1, e2) => true
+      case Prod(e1, e2) => false
+    }
+
+  override def toString =
+    this match {
+      case Num(n) => n.toString
+      case Sum(e1, e2) => e1 + " + " + e2
+      case Prod(e1, e2) => {
+        (if (e1.isSum) "(" + e1 + ")" else e1) + " * " +
+          (if (e2.isSum) "(" + e2 + ")" else e2)
+      }
+    }
 }
 
 
